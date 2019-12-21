1619d735b2f8fd4d61641d3102676df46a28089c
Ajay Shenoy <ajayshenoyh@gmail.com>
2019-12-21 13:02:31 -0800

    testing ansible pull

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -5,5 +5,6 @@
 </head>
 <body>
     <h1><?php echo "Welcome to devOps on AWS. This text was generated using php 7"?></h1>
+    <h2><?php echo "This change is reflected with anisble pull"?></h2>
     </body>
     </html>
